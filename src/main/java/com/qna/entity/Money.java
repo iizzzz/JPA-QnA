59e79dbc1727091db7164852b78fda5f686ce3cd
treewww <121031431+treewww@users.noreply.github.com>
2023-01-02 03:55:54 +0900

    feat: Add Coffee (#11)
    Co-authored-by: treeww <replicaset02@github.com>

---
+++ src/main/java/com/qna/entity/Money.java
@@ -0,0 +1,17 @@
+package com.qna.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Money {
+    private Integer value;
+}
