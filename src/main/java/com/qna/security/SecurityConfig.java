3deca9cd1a4db27da6a2de2f137a23f760a93bb3
treewww <replicaset02@gmail.com>
2023-01-09 07:48:26 +0900

    feat: JWT Token Login 구현

--- src/main/java/com/qna/security/SecurityConfig.java
+++ src/main/java/com/qna/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.qna.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,26 +18,38 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtTokenizer jwtTokenizer;
+
+    public SecurityConfig(JwtTokenizer jwtTokenizer) {
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    // HTTP 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .headers().frameOptions().sameOrigin() // (1)
+                .headers().frameOptions().sameOrigin()
                 .and()
-                .csrf().disable()        // (2)
-                .cors(withDefaults())    // (3)
-                .formLogin().disable()   // (4)
-                .httpBasic().disable()   // (5)
+                .csrf().disable()
+                .cors(withDefaults())
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomFilterConfigurer()) // JWT 로그인 설정
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()                // (6)
+                        .anyRequest().permitAll()
                 );
         return http.build();
     }
 
+    // PasswordEncoder 설정
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    // CORS 설정
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -45,5 +59,25 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    // JWTAuthenticationFIlter 등록
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+
+        @Override // Configure Method를 Override 함으로써 SecurityConfig를 Customizing 할 수 있다
+        public void configure(HttpSecurity builder) throws Exception {
+
+            // getSharedObject()로 인해 SecurityConfigurer간에 공유되는 객체를 얻을 수 있음
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            // 객체 생성과 동시에 AuthenticationManager & JwtTokenizer를 DI 해줌
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+
+            // setFilterProccessUrl() 를 통해 로그인 URL Customize 가능 (default url은 /login 이다)
+            jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+
+            // addFilter()를 통해 JWT 검증필터를 Filter Chain에 추가
+            builder.addFilter(jwtAuthenticationFilter);
+        }
     }
 }
