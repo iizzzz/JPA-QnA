0da60c87a8b36b07c9718891a6f0c5b74aa8232e
treewww <replicaset02@gmail.com>
2023-01-30 15:34:02 +0900

    docs: "Add Explains"
    Co-authored-by: treewww <121031431+treewww@users.noreply.github.com>

--- src/main/java/com/qna/security/CustomUserDetailsService.java
+++ src/main/java/com/qna/security/CustomUserDetailsService.java
@@ -13,6 +13,12 @@ import org.springframework.stereotype.Component;
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * 로그인 인증 처리
+ * DB에서 사용자의 크리덴셜 조회 후,
+ * 조회한 크리덴셜을 AuthenticationManager에게 전달하는 클래스
+ */
+
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -32,6 +38,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         Member findMember = optMember.orElseThrow(() ->
                 new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
 
+        // DB에 저장된 Role을 기반으로 권한 생성
         Collection<? extends GrantedAuthority> authorities = authorityUtils.createAuthorities(findMember.getRoles());
 
         return new CustomUserDetails(findMember);
