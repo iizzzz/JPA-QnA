9348a1a2d432bf94f4c5f52af72bf15a311b0d9c
roal <replicaset01@github.com>
2023-02-01 07:01:39 +0900

    fix: "조회 쿼리 추가"
    Co-authored-by: treewww <replicaset02@gmail.com>

--- src/main/java/com/qna/repository/QuestionRepository.java
+++ src/main/java/com/qna/repository/QuestionRepository.java
@@ -2,13 +2,18 @@ package com.qna.repository;
 
 import com.qna.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+    @Modifying
+    @Query("update Question q set q.views = q.views + 1 where q.questionId = :questionId")
+    int updateViews(@Param("questionId") Long questionId);
+
     @Query(value = "SELECT q FROM Question q WHERE q.questionId = :questionId")
     Optional<Question> findByOne(Long questionId);
-
 }
