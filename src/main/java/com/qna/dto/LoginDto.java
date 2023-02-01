3e158b04bee56fccd3731816cf632e41a041e6bb
roal <replicaset01@github.com>
2023-02-01 18:02:27 +0900

    feat: "LoginDTO의 응답 Response Body 생성"
    Co-authored-by: treewww <replicaset02@gmail.com>

--- src/main/java/com/qna/dto/LoginDto.java
+++ src/main/java/com/qna/dto/LoginDto.java
@@ -1,9 +1,19 @@
 package com.qna.dto;
 
+import com.qna.entity.contant.Role;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class LoginDto {
     private String email;
     private String password;
+
+    public static class Response {
+        private long memberId;
+        private String email;
+        private String name;
+        private List<Role> roles;
+    }
 }
