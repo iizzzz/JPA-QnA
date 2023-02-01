e3f3ed0fb33c0e929082215099a0723e6fd22cb8
roal <replicaset01@github.com>
2023-02-01 15:43:50 +0900

    feat: "삭제된 회원 제외 검색 메서드 추가"
    Co-authored-by: treewww <121031431+treewww@users.noreply.github.com>

--- src/main/java/com/qna/repository/QuestionRepository.java
+++ src/main/java/com/qna/repository/QuestionRepository.java
@@ -1,11 +1,15 @@
 package com.qna.repository;
 
 import com.qna.entity.Question;
+import com.qna.entity.contant.QuestionStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
@@ -16,4 +20,6 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 
     @Query(value = "SELECT q FROM Question q WHERE q.questionId = :questionId")
     Optional<Question> findByOne(Long questionId);
+
+    Page<Question> findAllByDeletedAtIsNull(Pageable pageable);
 }
