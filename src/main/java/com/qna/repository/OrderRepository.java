544f1e7ade0547c2a2349415d4db557f038e1e95
iizzzz <replicaset02@github.com>
2023-02-09 14:09:59 +0900

    초기화

--- src/main/java/com/qna/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qna.repository;
-
-import com.qna.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
